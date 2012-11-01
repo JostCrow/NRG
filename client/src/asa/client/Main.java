013e62c83a70eba66bd204387c7fae7c5dd22588
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2012-11-01 10:32:45 +0100

    removed layout style

--- client/src/asa/client/Main.java
+++ client/src/asa/client/Main.java
@@ -1,29 +1,13 @@
 package asa.client;
 
 import asa.client.frames.FullScreenJFrame;
-import asa.client.frames.NewJFrame;
 import gnu.io.CommPortIdentifier;
 import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
-		try {
-			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-				if ("GTK+".equals(info.getName())) {
-					javax.swing.UIManager.setLookAndFeel(info.getClassName());
-					break;
-				}
-			}
-		} catch (ClassNotFoundException ex) {
-			java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (InstantiationException ex) {
-			java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (IllegalAccessException ex) {
-			java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
-			java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-		}
+		
 		System.out.println("ASA client application");
 		
 		List<CommPortIdentifier> availablePorts = ComPortReader.getComPorts();
