4bc4af3e83793c689e54785dfc15e899f00aa8a5
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2012-11-28 13:31:12 +0100

    Genetated getters and setters for Device added a getAll for the DeviceTable

--- server/src/java/DBConnection/MySQL/DeviceTable.java
+++ server/src/java/DBConnection/MySQL/DeviceTable.java
@@ -1,6 +1,14 @@
 package DBConnection.MySQL;
 
+import Domain.Device;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DeviceTable {
 	
@@ -10,5 +18,27 @@ public class DeviceTable {
 		this.connection = connection;
 	}
 	
-	public 
+	public List<Device> getAll(){
+		List<Device> devices = new ArrayList<Device>();
+		try {
+			Statement statement = connection.createStatement();
+			
+			String query = "SELECT *"
+					+ "FROM device";
+			
+			ResultSet resultSet = statement.executeQuery(query);
+			
+			while(resultSet.next()){
+				Device d = new Device(resultSet.getInt("id"), resultSet.getString("name"), resultSet.getString("photo_url"), resultSet.getDouble("average_watt"), resultSet.getString("sensor"));
+				devices.add(d);
+			}
+			statement.close();
+			
+		} catch (SQLException ex) {
+			Logger.getLogger(DeviceTable.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
+		
+		return devices;
+	}
 }
--- server/src/java/Domain/Device.java
+++ server/src/java/Domain/Device.java
@@ -1,7 +1,52 @@
 package Domain;
 
 public class Device {
-	int id;
-	String name;
+	private int id;
+	private String name;
+	private String photo_url;
+	private double average_watt;
+	private String sensor;
 	
+	public Device(){
+	}
+
+	public Device(int id, String name, String photo_url, double average_watt, String sensor) {
+		this.id = id;
+		this.name = name;
+		this.photo_url = photo_url;
+		this.average_watt = average_watt;
+		this.sensor = sensor;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoto_url() {
+		return photo_url;
+	}
+
+	public void setPhoto_url(String photo_url) {
+		this.photo_url = photo_url;
+	}
+
+	public double getAverage_watt() {
+		return average_watt;
+	}
+
+	public void setAverage_watt(double average_watt) {
+		this.average_watt = average_watt;
+	}
+
+	public String getSensor() {
+		return sensor;
+	}
+
+	public void setSensor(String sensor) {
+		this.sensor = sensor;
+	}
 }
