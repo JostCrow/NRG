78adac0799638a43817ac1450562829bf7db397c
Rene van Aere <info@rvgate.nl>
2012-11-29 14:28:44 +0100

    Added keyboard input, icons, backgrounds

--- client/src/asa/client/InfoState.java
+++ client/src/asa/client/InfoState.java
@@ -1,22 +1,37 @@
 package asa.client;
+import java.util.Arrays;
+import java.util.List;
+
 import org.lwjgl.util.Dimension;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 import asa.client.resources.Resource;
 
-public class InfoState extends BasicGameState {
+public class InfoState extends ArduinoGameState {
 
-	int stateID = -1;
 	Image tandwiel1;
 	Image tandwiel2;
 	Image background;
 	Image spinner;
 	Image background_spinner;
+	Image spinneroverlay;
+	
+	List<WheelOption> wheelOptions = Arrays.asList(
+			new WheelOption("Koffie", Resource.ICON_KOFFIE, Resource.BACKGROUND_KOFFIE),
+			new WheelOption("Printer", Resource.ICON_PRINTER, Resource.BACKGROUND_PRINTER),
+			new WheelOption("Koffie", Resource.ICON_KOFFIE, Resource.BACKGROUND_KOFFIE),
+			new WheelOption("Printer", Resource.ICON_PRINTER, Resource.BACKGROUND_PRINTER),
+			new WheelOption("Koffie", Resource.ICON_KOFFIE, Resource.BACKGROUND_KOFFIE),
+			new WheelOption("Printer", Resource.ICON_PRINTER, Resource.BACKGROUND_PRINTER),
+			new WheelOption("Koffie", Resource.ICON_KOFFIE, Resource.BACKGROUND_KOFFIE),
+			new WheelOption("Printer", Resource.ICON_PRINTER, Resource.BACKGROUND_PRINTER),
+			new WheelOption("Cola", Resource.ICON_AUTOMAAT, Resource.BACKGROUND_AUTOMAAT)
+	);
+			
 	
 	int targetrotation = 0;
 	int tandwielOffset = 30;
@@ -27,16 +42,10 @@ public class InfoState extends BasicGameState {
 	Dimension screenSize;
 	
 	public InfoState(int stateID) {
-		this.stateID = stateID;
-	}
-
-	@Override
-	public int getID() {
-		return stateID;
+		super(stateID);
 	}
 
 	public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-		Arduino arduino = Arduino.getInstance();
 		arduino.addListener(new ArduinoAdapter() {
 			@Override
 			public void wheelEvent(int direction, int speed) {
@@ -51,8 +60,9 @@ public class InfoState extends BasicGameState {
 		tandwiel1 = new Image(Resource.getPath(Resource.TANDWIEL5));
 		tandwiel2 = new Image(Resource.getPath(Resource.TANDWIEL6));
 		spinner = new Image(Resource.getPath(Resource.SPINNER));
+		spinneroverlay = new Image(Resource.getPath(Resource.SPINNER_OVERLAY));
 		background_spinner = new Image(Resource.getPath(Resource.BACKGROUND_SPINNER));
-		background = new Image(Resource.getPath(Resource.BACKGROUND_KOFFIE));
+		background = new Image(Resource.getPath(Resource.BACKGROUND_BEAMER));
 		
 	}
 
@@ -61,10 +71,31 @@ public class InfoState extends BasicGameState {
 		tandwiel1.draw(-tandwiel1.getWidth() / 2, gameContainer.getHeight() / 2 - tandwiel1.getHeight() / 2);
 		tandwiel2.draw(tandwiel1.getWidth()/2-tandwielOffset-40, gameContainer.getHeight() / 2 - tandwiel2.getHeight());
 		spinner.draw(gameContainer.getWidth() / 2 - spinner.getWidth() / 2 - 100, gameContainer.getHeight() / 2 - spinner.getHeight() / 2);
+		spinneroverlay.draw(gameContainer.getWidth() / 2 - spinner.getWidth() / 2 - 100, gameContainer.getHeight() / 2 - spinner.getHeight() / 2);
 		background_spinner.draw(gameContainer.getWidth() / 2 - background_spinner.getWidth() / 2 - 100, gameContainer.getHeight() / 2 - background_spinner.getHeight() / 2);
+		
+		Dimension center = new Dimension(gameContainer.getWidth() / 2 - 100, gameContainer.getHeight() / 2);
+		
+		
+		for(int i = 0; i < wheelOptions.size(); i++){
+			float offsetDegree = 360/wheelOptions.size()*i;
+			float degrees = (rotation + offsetDegree) % 360;
+			float rad = (float) (degrees * (Math.PI / 180));
+			float radius = 310;
+			
+			float x = (float) (center.getWidth() + radius * Math.cos(rad));
+			float y = (float) (center.getHeight() + radius * Math.sin(rad));
+			
+			WheelOption option = wheelOptions.get(i);
+			Image optionIcon = option.getIcon();
+			x = x - optionIcon.getWidth()/2;
+			y = y - optionIcon.getHeight()/2;
+			option.getIcon().draw(x, y);
+		}
 	}
 
 	public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
+		super.update(gameContainer, stateBasedGame, delta);
 		rotation += (targetrotation - rotation) / rotationEase;
 		tandwiel1.setRotation(rotation);
 		tandwiel2.setRotation((float) ((float) -(rotation*1.818181818181818)+16.36363636363636));
--- client/src/asa/client/resources/Resource.java
+++ client/src/asa/client/resources/Resource.java
@@ -5,24 +5,35 @@ import java.net.URL;
 public class Resource {
 
 	public static final String RESOURCE_PATH = "/asa/client/resources/";
-	public static final String LOGCONFIG = RESOURCE_PATH + "log4j.properties";
-	public static final String BACKGROUND = "../src/asa/client/resources/gearsanimation/00.png";
-
-	public static final String TANDWIEL1 = RESOURCE_PATH + "tandwiel1.png";
-	public static final String TANDWIEL2 = RESOURCE_PATH + "tandwiel2.png";
-	public static final String TANDWIEL3 = RESOURCE_PATH + "tandwiel3.png";
-	public static final String TANDWIEL4 = RESOURCE_PATH + "tandwiel4.png";
-	public static final String TANDWIEL5 = RESOURCE_PATH + "tandwiel5.png";
-	public static final String TANDWIEL6 = RESOURCE_PATH + "tandwiel6.png";
 	
-	public static final String BACKGROUND_KOFFIE = RESOURCE_PATH + "background_koffie.png";
-	public static final String SPINNER = RESOURCE_PATH + "spinner.png";
-	public static final String BACKGROUND_SPINNER = RESOURCE_PATH + "background_spinner.png";
+	public static final String LOGCONFIG = "log4j.properties";
+
+	public static final String TANDWIEL1 = "tandwiel1.png";
+	public static final String TANDWIEL2 = "tandwiel2.png";
+	public static final String TANDWIEL3 = "tandwiel3.png";
+	public static final String TANDWIEL4 = "tandwiel4.png";
+	public static final String TANDWIEL5 = "tandwiel5.png";
+	public static final String TANDWIEL6 = "tandwiel6.png";
+	
+	public static final String BACKGROUND_KOFFIE = "background_koffie.png";
+	public static final String BACKGROUND_PRINTER = "background_printer.png";
+	public static final String BACKGROUND_AUTOMAAT = "background_cola.png";
+	public static final String BACKGROUND_BEAMER = "background_beamert.png";
+	
+	public static final String ICON_KOFFIE = "icon_koffie.png";
+	public static final String ICON_PRINTER = "icon_printer.png";
+	public static final String ICON_AUTOMAAT = "icon_automaat.png";
+	public static final String ICON_BEAMER = "icon_beamer.png";
+	
+	public static final String SPINNER = "spinner.png";
+	public static final String SPINNER_OVERLAY = "spinneroverlay.png";
+	public static final String BACKGROUND_SPINNER = "background_spinner.png";
+	
 	
 	public static URL getURL(String path) {
 		URL resourceUrl = null;
 		try {
-			resourceUrl = Resource.class.getResource(path);
+			resourceUrl = Resource.class.getResource(RESOURCE_PATH + path);
 		} catch (Exception exception) {
 
 		}
@@ -30,6 +41,6 @@ public class Resource {
 	}
 	
 	public static String getPath(String path){
-		return "C:/Users/Rene van Aerle/Git/PhysicASA/client/src" + path;
+		return "C:/Users/Rene van Aerle/Git/PhysicASA/client/src" + RESOURCE_PATH + path;
 	}
 }
