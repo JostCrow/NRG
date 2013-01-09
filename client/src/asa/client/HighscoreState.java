bccc1f9ff75748bc36653564ddb287261cd2270b
Corneel92 <corneel.jansen@student.hu.nl>
2013-01-09 13:31:32 +0100

    updated drawing of the centerimage in hingscorelist

--- client/src/asa/client/HighscoreState.java
+++ client/src/asa/client/HighscoreState.java
@@ -317,7 +317,6 @@ public class HighscoreState extends ArduinoGameState implements ImageObserver{
 			overlay_selected.draw(appResWidth - appResWidth / 4 - appResWidth / 8, appResHeight / 2 - overlay_selected.getHeight() / 2);
 			
 			getCenterImage();
-			centerImage.draw(center.getWidth() - centerImage.getWidth() / 2, center.getHeight() - centerImage.getHeight());
 			graphics.drawString(decimalFormat.format(highscores.get(selected).getScore()), center.getWidth(), center.getHeight());
 			graphics.drawString(decimalFormat.format(gameData.getDeviceScore()), center.getWidth(), center.getHeight() + 100);			
 			
@@ -538,12 +537,15 @@ public class HighscoreState extends ArduinoGameState implements ImageObserver{
 			File f = new File(highscores.get(selected).getId() + ".png");
 			if(f.exists()){
 				centerImage = new Image(highscores.get(selected).getId() + ".png");
+				centerImage.getSubImage(50, 0, 540, 480).draw(center.getWidth()-((640-50)/2)+20, center.getHeight()-(480/2)-20);
 			} else {
 				centerImage = new Image(Resource.getPath("avatar.png"));
+				centerImage.draw(center.getWidth() - centerImage.getWidth() / 2, center.getHeight() - centerImage.getHeight());
 			}
 		}
 		else{
 			centerImage = new Image(Resource.getPath("avatar.png"));
+			centerImage.draw(center.getWidth() - centerImage.getWidth() / 2, center.getHeight() - centerImage.getHeight());
 		}
 	}
 }
